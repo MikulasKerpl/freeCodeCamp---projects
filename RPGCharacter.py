30ed68e5d487eb072c7e7680e31833c67b279348
Mikuláš Kerpl <mikulas@Mikulas--MacBook-Air.local>
2026-08-08 15:31:31 +0200

    initial commit

---
+++ RPGCharacter.py
@@ -0,0 +1,28 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(character_name, strength, intelligence, charisma):
+    if not isinstance(character_name, str):
+        return "The character name should be a string"
+    if character_name == "":
+        return "The character should have a name"
+    if len(character_name) > 10:
+        return "The character name is too long"
+    if " " in character_name:
+        return "The character name should not contain spaces"
+    if not all(isinstance(stat, int) for stat in (strength, intelligence, charisma)):
+        return "All stats should be integers"
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no more than 4"
+    sum = strength + intelligence + charisma
+    if sum != 7:
+        return "The character should start with 7 points"
+    return (
+        f"{character_name}\n"
+        f"STR {full_dot * strength}{empty_dot * (10 - strength)}\n"
+        f"INT {full_dot * intelligence}{empty_dot * (10 - intelligence)}\n"
+        f"CHA {full_dot * charisma}{empty_dot * (10 - charisma)}"
+    )
+
